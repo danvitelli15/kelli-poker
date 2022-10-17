534d2a29dd4e4bd415bc997ec650d8a29a583560
Daniel Vitelli <danvitelli15@gmail.com>
2022-10-17 00:18:20 -0500

    fixed prod log thing

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,8 +1,13 @@
 import pino from "pino";
 
+const transport =
+  process.env.PRETTY_LOGS === "true"
+    ? { transport: { options: { colorize: true, messageFormat: "{service} | {msg}" }, target: "pino-pretty" } }
+    : {};
+
 export const baseLogger = pino({
   level: process.env.LOG_LEVEL || "info",
-  transport: { options: { colorize: true, messageFormat: "{service} | {msg}" }, target: "pino-pretty" },
+  ...transport,
 });
 
 export const loggerFactory = (service: string) => baseLogger.child({ service });
