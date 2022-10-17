55cdb682aa421e461249a32189a6c09b14ef5c78
Daniel Vitelli <danvitelli15@gmail.com>
2022-10-16 23:49:56 -0500

    add bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import Head from "next/head";
+import { useEffect } from "react";
+
+import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return (
     <>
       <Head>
