3312c2dee327ec4606445cb75ea28d42115928b0
Daniel Vitelli <danvitelli15@gmail.com>
2022-10-31 10:23:00 -0500

    turned off password checking for testing

--- pages/account/create.tsx
+++ pages/account/create.tsx
@@ -11,14 +11,14 @@ interface ICreateAccountFormData {
 }
 
 export const CreateAccountPage = () => {
-  const [isMatching, setIsMatching] = useState(true);
+  // const [isMatching, setIsMatching] = useState(true);
 
   const onSubmit = useCallback((form: ICreateAccountFormData, event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (form.password !== form.confirmPassword) {
-      setIsMatching(false);
-      return;
-    }
+    // if (form.password !== form.confirmPassword) {
+    //   setIsMatching(false);
+    //   return;
+    // }
     fetch("/api/account/create", { body: JSON.stringify(form), method: "POST" }).then((response) =>
       response.status === 200 ? response.json().then((body) => window.location.replace(body.goTo)) : null
     );
@@ -33,7 +33,7 @@ export const CreateAccountPage = () => {
         <TextField identifier="lastName" label="Last Name" />
         <TextField identifier="displayName" label="Display Name" />
         <br />
-        {!isMatching && <p>Passwords do not match</p>}
+        {/* {!isMatching && <p>Passwords do not match</p>} */}
         <PasswordField identifier="password" label="Password" />
         <PasswordField identifier="confirmPassword" label="Confirm Password" />
         <SubmitButton className="btm btn-primary">Create Account</SubmitButton>
